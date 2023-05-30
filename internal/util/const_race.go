f7c8ed7db1a96b29d8f3dd93f53d318922c64f64
amyrazz44 <amybai6666@gmail.com>
2023-05-30 15:33:18 +0800

    Compatible with mxstop and gpstop

--- internal/util/const_race.go
+++ internal/util/const_race.go
@@ -9,7 +9,6 @@ const (
 	CLI_BIN           = "mxbench_race"
 	MX_GATE_CLI_BIN   = "mxgated"
 	GP_CONFIG_CLI_BIN = "gpconfig"
-	GP_STOP_CLI_BIN   = "gpstop"
 	CREATEDB_CLI_BIN  = "createdb"
 
 	TIME_FMT         = "2006-01-02 15:04:05"
--- internal/util/gpstop.go
+++ internal/util/gpstop.go
@@ -1,6 +1,18 @@
 package util
 
+import "fmt"
+
 func RestartDB() error {
-	_, err := runCmdAndDealingWithError(GP_STOP_CLI_BIN, "-iraq")
+	outStr, err := runCmdAndDealingWithError(PSQL_BIN, "-c", "show mx_ha_provider")
+	if err != nil || outStr == "" {
+		return fmt.Errorf("fail to show mx_ha_provider %v", err)
+	}
+	var STOP_CLI_BIN string
+	if outStr == "external" {
+		STOP_CLI_BIN = MX_STOP_CLI_BIN
+	} else {
+		STOP_CLI_BIN = GP_STOP_CLI_BIN
+	}
+	_, err = runCmdAndDealingWithError(STOP_CLI_BIN, "-iraq")
 	return err
 }
