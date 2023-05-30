50bffc4ca8d63d835ad853c3ad8715ed31e902d4
Bai Jie <amybai6666@gmail.com>
2023-05-30 15:59:02 +0800

    Make command line tools compatible with different YMatrix version  (#27)

--- internal/util/const.go
+++ internal/util/const.go
@@ -8,7 +8,9 @@ const (
 	MX_GATE_CLI_BIN   = "mxgated"
 	GP_CONFIG_CLI_BIN = "gpconfig"
 	GP_STOP_CLI_BIN   = "gpstop"
+	MX_STOP_CLI_BIN   = "mxstop"
 	CREATEDB_CLI_BIN  = "createdb"
+	PSQL_BIN          = "psql"
 
 	TIME_FMT         = "2006-01-02 15:04:05"
 	TIME_WITH_TZ_FMT = "2006-01-02 15:04:05 -0700"
